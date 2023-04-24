63762f75e88bacca56217d47bd84ea3e2044e767
gurumukh <gurmukh.singh@bertelsmann.de>
2023-04-24 16:51:25 +0800

    update selenium test

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -12,7 +12,7 @@ def login(user, password):
     options = ChromeOptions()
     options.add_argument("--headless")
     driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome("/usr/bin/chromedriver")
     print("Browser started successfully. Navigating to the demo page to login.")
     driver.get("https://www.saucedemo.com/")
 

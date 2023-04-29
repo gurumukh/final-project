c0eaec04cf3cc6c853d312c5c02163bfe1f188af
gurumukh <gurmukh.singh@bertelsmann.de>
2023-04-29 16:11:56 +0800

    update pipeline

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -1,7 +1,10 @@
 # #!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.options import Options
+
+
+d = webdriver.Chrome(chrome_options=chrome_options)
 import time
 
 
@@ -9,13 +12,11 @@ import time
 def login(user, password):
     print("Starting the browser...")
     # --uncomment when running in Azure DevOps.
-    options = ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
 
-    driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome("/usr/local/bin/chromedriver")
+    driver = webdriver.Chrome(chrome_options=chrome_options)
     print("Browser started successfully. Navigating to the demo page to login.")
     driver.get("https://www.saucedemo.com/")
 

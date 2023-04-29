394a70f8e15ba6146775e4f0d2fcd0c34456b276
gurumukh <gurmukh.singh@bertelsmann.de>
2023-04-29 14:55:55 +0800

    update selenium test

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -10,9 +10,10 @@ def login(user, password):
     print("Starting the browser...")
     # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--headless")
+
     driver = webdriver.Chrome(options=options)
     driver = webdriver.Chrome("/usr/local/bin/chromedriver")
     print("Browser started successfully. Navigating to the demo page to login.")
